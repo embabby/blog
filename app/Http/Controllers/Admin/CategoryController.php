b5c082dfd8f211fbb1cb12a486ea31388b431006
embaby <mohamed.nabil@at-portal.info>
2018-12-27 13:37:35 +0200

    admin panel set up

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Category;
+
+class CategoryController extends Controller
+{
+    
+    public function index()
+    {
+      $categories = Category::all();
+      return view('admin.categories.all', compact('categories'));
+    }
+
+    
+    public function create()
+    {
+        return view('admin.categories.create');
+    }
+
+    
+    public function store(Request $request)
+    {   
+        Category::create([
+          'name' => $request->name
+        ]);
+        return redirect()->route('category.index')->with('message','Category Created Successfully');
+    }
+
+    
+    public function show($id)
+    {
+        
+    }
+
+   
+    public function edit($id)
+    {
+      $category = Category::find($id);
+      return view('admin.categories.edit', compact('category'));
+    }
+
+    
+    public function update(Request $request, $id)
+    {
+      $category = Category::find($id);
+      
+      $category->update([
+          'name' => $request->name
+      ]);
+
+      return redirect()->route('category.index')->with('message','Category Edited Successfully');
+    }
+
+    
+    public function destroy($id)
+    {
+
+    }
+
+
+    public function getDelete($id)
+    {
+
+      $category = Category::find($id);
+      $category->delete();
+      return redirect()->route('category.index')->with( 'message','Category Had Been Deleted');
+    }
+}
